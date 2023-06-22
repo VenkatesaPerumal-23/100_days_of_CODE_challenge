4eed2c5d809581e2a655ea0ca5c1de4ea991d6ed
VenkatesaPerumal-23 <venkatphoenix04@gmail.com>
2023-06-22 13:06:49 +0530

    Initial commit

---
+++ day_0.py
@@ -0,0 +1,22 @@
+#combination_add
+num1=int(input("enter num1:"))
+num2=int(input("enter num2:"))
+num3=int(input("enter num3:"))
+print(num1+num2)
+print(num1+num3)
+print(num2+num3)
+
+#area_of_circle
+pi=3.14
+radius=int(input("Enter radius value:"))
+area=pi*radius**2
+print(area)
+
+#odd_or_even_check
+
+number=int(input("Enter the number:"))
+if(number%2==0):
+    print("Yes")
+else:
+    print("No")
+
