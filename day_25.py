c82efbc51a904b5b88650f6259c06e85e0efce2c
VenkatesaPerumal <122686745+VenkatesaPerumal-23@users.noreply.github.com>
2023-07-17 19:00:49 +0530

    Add files via upload

---
+++ day_25.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def printSquare(self, N):
+        for i in range(N):
+            print('* '*N)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ob.printSquare(N)
+# } Driver Code Ends
