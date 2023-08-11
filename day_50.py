83e6d49f20c18e46de917312670e6da2483ff455
VenkatesaPerumal <122686745+VenkatesaPerumal-23@users.noreply.github.com>
2023-08-11 19:23:32 +0530

    Add files via upload

---
+++ day_50.py
@@ -0,0 +1,25 @@
+#rotate array elements by d times
+
+class Solution:
+    def leftRotate(self, arr, n, d):
+        arr[0:]=arr[d:]+arr[:d]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        d = int(input())
+        ob = Solution()
+        ob.leftRotate(arr, n, d)
+        for xx in arr:
+            print(xx, end=" ")
+        print()
+        tc -= 1
+
+# } Driver Code Ends
