55dde2ae7f415a4bf337d377b61191c5c8b37459
VenkatesaPerumal <122686745+VenkatesaPerumal-23@users.noreply.github.com>
2023-09-23 08:20:48 +0530

    Add files via upload

---
+++ day_92.py
@@ -0,0 +1,22 @@
+#Single Non-duplicate Element
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        if(len(nums)==1):
+            return nums[0]
+        if(nums[0]!=nums[1]):
+            return nums[0]
+        if(nums[len(nums)-1]!=nums[len(nums)-2]):
+            return nums[len(nums)-1]
+        low=1 
+        high=len(nums)-2 
+        while(low<=high):
+            mid=(low+high)//2 
+            if(nums[mid]!=nums[mid-1] and nums[mid]!=nums[mid+1]):
+                return nums[mid] 
+            
+            if((mid%2==0 and nums[mid]==nums[mid+1]) or (mid%2==1 and nums[mid]==nums[mid-1])):
+               low=mid+1
+            else:
+               high=mid-1 
+               
+        return -1
